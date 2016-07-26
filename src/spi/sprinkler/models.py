ab8ed9cfdbe48ef4bbb81457657b1d48dd1a2de1
Adrian Breazu <breazuadrian@gmail.com>
2016-07-26 04:42:15 -0700

    add db tables

--- src/spi/sprinkler/models.py
+++ src/spi/sprinkler/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Scheduler(models.Model):
+    id = models.IntegerField(unique=True, primary_key="True")
+    name = models.CharField(max_length=200)
+    start_time = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Sprinkler(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    name = models.CharField(max_length=200)
+    duration = models.IntegerField()
+    order = models.IntegerField()
+    scheduler_id = models.ForeignKey(Scheduler, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
