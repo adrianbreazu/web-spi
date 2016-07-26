7fc70c4a99349e5e7803ea68e3366e8d53950db4
Adrian Breazu <breazuadrian@gmail.com>
2016-07-26 13:52:51 -0700

    init index web page

--- src/spi/spi/settings.py
+++ src/spi/spi/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'spi.urls'
 
+APPS_DIRS = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
--- src/spi/sprinkler/views.py
+++ src/spi/sprinkler/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Scheduler,Sprinkler
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the index page")
+    try:
+        scheduler = Scheduler.objects.get(pk=1)
+        sprinkler_list = Sprinkler.objects.order_by('order')
+        context = {
+            "scheduler": scheduler,
+            "sprinkler_list": sprinkler_list,
+        }
+    except:
+        raise Http404("This is the page you are looking for")
+    return render(request=request, template_name="sprinkler/index.html", context=context)
