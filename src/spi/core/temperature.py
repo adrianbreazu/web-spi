baf7d750d5aa801d09cbc3e2c6786732983afd37
Adrian Breazu <breazuadrian@gmail.com>
2016-09-03 00:40:11 +0300

    test will be deleted

--- src/spi/core/temperature.py
+++ src/spi/core/temperature.py
@@ -1,28 +1,41 @@
-import core.Adafruit_Python_DHT.Adafruit_DHT as dht_sensor
-import logging
+import Adafruit_DHT as dht_sensor
+import sqlite3 as sqlite
+import datetime as datetime
+import random as random
 
-logger = logging.getLogger(__name__)
-SENSORTYPE = dht_sensor.DHT22
+SENSORTYPE = 22 #dht_sensor.DHT22
 GPIO_PIN = 17
-
+DB = '/var/www/web-spi/src/spi/db.sqlite3'
+#DB = '/home/adrianbreazu/repositories/web-spi/src/spi/db.sqlite3'
 
 def get_readings():
-    logger.debug("beginning of get_readings")
-    temperature = 0;
-    humidity = 0;
-
     try:
-        humidity, temperature = dht_sensor.read_retry(SENSORTYPE, GPIO_PIN)
-
+        humidity, temperature = dht_sensor.read_retry(sensor=SENSORTYPE, pin=GPIO_PIN, platform="RASPBERRY_PI")
         reads = {}
-        reads['temperature'] = str(temperature)
-        reads['humidity'] = str(humidity)
-        logger.debug("set temperature and humidity of get_readings: {0:0.1f} *C | {1:0.1f} %".format(temperature, humidity))
+        connection = sqlite.connect(DB)
+        cursor = connection.cursor()
+
+        if (humidity is not None) & (temperature is not None):
+            reads['temperature'] = round(temperature, 2)
+            reads['humidity'] = round(humidity, 2)
+
+        cursor.execute("INSERT INTO sprinkler_weather VALUES ( ?, ?, ?, ?)", (random.randint(1, 1000000000), humidity, temperature, str(datetime.datetime.now().strftime("%H:%M:%S"))))
+        connection.commit()
+
+    except sqlite.Error as e:
+        print("Error: {0]".format(e.args[0]))
+
     except Exception as e:
-        logger.error("get_readings exception {0}".format(e))
+        print ("exception: {0]".format(e))
         reads['temperature'] = 0
         reads['humidity'] = 0
+
     finally:
-        logger.debug("end of get_readings: {0:0.1f} *C | {1:0.1f} %".format(temperature, humidity))
+        connection.close()
+        print(555)
         return reads
 
+
+if __name__ == "__main__":
+    data = get_readings()
+    print("{0}".format(data))
